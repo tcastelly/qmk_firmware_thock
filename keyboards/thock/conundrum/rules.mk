49e230bf8fd230be978135d1beaad4c925c96554
Thomas CASTELLY <thomas@castelly.fr>
2023-02-27 10:40:56 +0100

    remove include mouse

--- keyboards/thock/conundrum/rules.mk
+++ keyboards/thock/conundrum/rules.mk
@@ -20,7 +20,7 @@ CUSTOM_MATRIX = yes
 #   comment out to disable the options.
 #
 BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration(+1000)
-MOUSEKEY_ENABLE = yes        # Mouse keys(+4700)
+MOUSEKEY_ENABLE = no        # Mouse keys(+4700)
 EXTRAKEY_ENABLE = no # yes       # Audio control and System control(+450)
 CONSOLE_ENABLE = yes         # Console for debug(+400)
 COMMAND_ENABLE = no         # Commands for debug and configuration
