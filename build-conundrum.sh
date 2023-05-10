db5bec8e00b06e31262f5654e846c3f3c0b5eba8
Thomas CASTELLY <thomas@castelly.fr>
2023-05-10 08:49:56 +0200

    remove orphan container after build

--- build-conundrum.sh
+++ build-conundrum.sh
@@ -2,4 +2,4 @@
 set -exo pipefail
 
 docker build -t thock/conundrum .
-docker run -v $PWD:/qmk_firmware -v $PWD/.build:/qmk_firmware/.build -it thock/conundrum
+docker run --rm -v $PWD:/qmk_firmware -v $PWD/.build:/qmk_firmware/.build -it thock/conundrum
